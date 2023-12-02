00cb5a28f2fc3f3f64af6321d3628c512568d512
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-02 15:53:58 -0300

    implementacion

--- src/main/java/com/example/demo/repositories/ItemRepositoryImpl.java
+++ src/main/java/com/example/demo/repositories/ItemRepositoryImpl.java
@@ -6,7 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-public class ItemRepositoryImpl {
+/**
+ * We’ll provide an implementation class for ItemRepository next. The key part of this class name is 
+ * the Impl ending. It is not connected to Spring Data and it only implements ItemRepositoryCustom. When 
+ * injecting an ItemRepository bean, Spring Data will have to create a proxy class; it will detect that 
+ * ItemRepository implements ItemRepositoryCustom and will look up a class called ItemRepositoryImpl to act 
+ * as a custom repository implementation. Consequently, the methods of the injected ItemRepository bean will 
+ * have the same behavior as the methods of the ItemRepositoryImpl class.
+ * 
+ * Es decir, funciona para implementar los métodos de una interfaz como es ItemRepositoryCustom, interfaz que
+ * buscará implementarse cuando busque Spring crear el bean de ItemRepository. Dado que este último repo 
+ * implementa ItemRepositoryCustom. 
+ * 
+ * @author peter
+ *
+ */
+public class ItemRepositoryImpl implements ItemRepositoryCustom {
 
 	@Autowired
     private ItemRepository itemRepository;
