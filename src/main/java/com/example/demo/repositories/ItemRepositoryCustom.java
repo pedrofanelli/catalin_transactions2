8bc4b3e4e91c7d63cc53f483253c5d739574c83c
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-02 15:37:18 -0300

    fragment interface

--- src/main/java/com/example/demo/repositories/ItemRepositoryCustom.java
+++ src/main/java/com/example/demo/repositories/ItemRepositoryCustom.java
@@ -1,6 +1,23 @@
 package com.example.demo.repositories;
 
-public class ItemRepositoryCustom {
+import java.time.LocalDate;
 
-	
+/**
+ * Such an interface is known as a FRAGMENT INTERFACE, and its purpose is to extend a repository 
+ * with custom functionality, which will be provided by a later implementation.
+ * 
+ * @author peter
+ *
+ */
+public interface ItemRepositoryCustom {
+
+	void addItem(String name, LocalDate creationDate);
+
+    void checkNameDuplicate(String name);
+
+    void addLogs();
+
+    void showLogs();
+
+    void addItemNoRollback(String name, LocalDate creationDate);
 }
