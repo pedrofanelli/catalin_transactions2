9cbc1381cd882132c4d3448bbef4ea42cfc08459
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-02 16:06:50 -0300

    log model

---
+++ src/main/java/com/example/demo/models/Log.java
@@ -0,0 +1,55 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Log {
+
+	private int id;
+    private String message;
+    private LocalDate date;
+
+    public Log() {
+    }
+
+    public Log(String message) {
+        this.message = message;
+        this.date = LocalDate.now();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Log [message=" + message + ", date=" + date + "]";
+    }
+}
